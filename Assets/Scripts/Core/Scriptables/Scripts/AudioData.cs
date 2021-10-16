e92e0b4870c60f901bdc5917c8ff08b3a191bb5b
Dima Zangana <zanganadima@gmail.com>
2021-10-16 15:50:42 +0300

    Add AudioData.cs as ScriptableObject to contain AudioClips to play in link with PlayAudioClip.cs

---
+++ Assets/Scripts/Core/Scriptables/Scripts/AudioData.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Audio Data", menuName = "Audio Data/New Audio Data", order = 51)]
+public class AudioData : ScriptableObject
+{
+    public AudioClip[] ClipsToPlay;
+}
