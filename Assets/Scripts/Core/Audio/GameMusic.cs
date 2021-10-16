78e91a0ab67774640694d6ab1b0ff16837f63afd
Dima Zangana <zanganadima@gmail.com>
2021-10-16 15:48:09 +0300

    Add GameMusic.cs, PlayAudioClip

---
+++ Assets/Scripts/Core/Audio/GameMusic.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameMusic : MonoBehaviour
+{
+    public AudioClip FirstBeep;
+    public AudioClip SecondBeep;
+
+    private AudioSource MusicAudioSource;
+
+    private bool isMusicOn;
+
+    [SerializeField] private float MusicTimerDelay;
+    //public float MusicTimerSubtractor;
+
+    private const float MUSIC_TIME_DELAY_DEFAULT = 1.5f;
+    //private const float MUSIC_TIME_SUBTRACTOR_DEFAULT = 0;
+
+    private int CurrentAsteroidsLeft = 0;
+    private int InitialAsteroidsAmount = 0;
+
+    /// <summary>
+    /// New music volume is 0.5 + 0.5 * amountOfAsteroidsRightnow / initialAmountOfAsteroids
+    /// </summary>
+
+
+    private void Awake()
+    {
+        MusicAudioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnEnable()
+    {
+        Actions.OnAsteroidDestroyed += IncreaseMusicSpeed;
+    }
+
+    private void OnDisable()
+    {
+        Actions.OnAsteroidDestroyed -= IncreaseMusicSpeed;
+    }
+
+    public void ResetMusic()
+    {
+        StopAllCoroutines();
+        //MusicTimerDelay = MUSIC_TIME_DELAY_DEFAULT;
+    }
+
+    public void StartMusicForNewGame()
+    {
+        StartCoroutine(StartMusicTimer());
+        isMusicOn = true;
+    }
+
+    public void SetupMusicTimeSubtractor(int amountOfAsteroids)
+    {
+        ResetMusic();
+        CurrentAsteroidsLeft = amountOfAsteroids;
+        InitialAsteroidsAmount = amountOfAsteroids;
+        MusicTimerDelay = CalculateSubtraction();
+    }
+
+    private float CalculateSubtraction()
+    {
+        var result = MUSIC_TIME_DELAY_DEFAULT * CurrentAsteroidsLeft / InitialAsteroidsAmount + 0.5f;
+        return result;
+    }
+
+    public IEnumerator StartMusicTimer()
+    {
+        if (!MusicAudioSource.isPlaying)
+            MusicAudioSource.PlayOneShot(FirstBeep);
+
+        yield return new WaitForSeconds(MusicTimerDelay);
+
+        if (!MusicAudioSource.isPlaying)
+            MusicAudioSource.PlayOneShot(SecondBeep);
+
+        yield return new WaitForSeconds(MusicTimerDelay);
+
+        StartCoroutine(StartMusicTimer());
+    }
+
+    public void IncreaseMusicSpeed(AsteroidProjectile asteroidProjectile)
+    {
+        CurrentAsteroidsLeft--;
+        MusicTimerDelay = CalculateSubtraction();
+    }
+
+    public void StopMusic()
+    {
+        isMusicOn = false;
+        ResetMusic();
+    }
+}
---
+++ Assets/Scripts/Core/Audio/PlayAudioClip.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayAudioClip : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public AudioData SoundData;
+
+    public AudioClip[] SoundsToPlay;
+
+    public bool isPlayingOnEnable;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnValidate()
+    {
+        SoundsToPlay = SoundData.ClipsToPlay;
+    }
+
+    private void OnEnable()
+    {
+        if (isPlayingOnEnable)
+            PlayRandomClip();
+    }
+    public void PlayRandomClip()
+    {
+        AudioClip randomClip = GetRandomAudioClip();
+
+        PlaySound(randomClip);
+    }
+
+    private AudioClip GetRandomAudioClip()
+    {
+        int randomClip = Random.Range(0, SoundsToPlay.Length);
+        return SoundsToPlay[randomClip];
+    }
+
+    private void PlaySound(AudioClip soundToPlay)
+    {
+        audioSource.PlayOneShot(soundToPlay);
+    }
+}
