b6317e599c352a32b4d052dcaa1626a8de6e315d
Dima Zangana <zanganadima@gmail.com>
2021-10-16 14:11:42 +0300

    Add GameScore.cs

---
+++ Assets/Scripts/Core/Gameplay/GameScore.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class GameScore : MonoBehaviour
+{
+    private GameService GameService;
+
+    public int TotalScore { get; set; }
+
+    private void Awake()
+    {
+        GameService = GetComponent<GameService>();
+    }
+
+    private void OnEnable()
+    {
+        Actions.OnAsteroidDestroyed += AddPoints;
+    }
+    private void OnDisable()
+    {
+        Actions.OnAsteroidDestroyed -= AddPoints;
+    }
+
+    public void Initialize()
+    {
+        TotalScore = 0;
+    }
+
+    public void AddPoints(AsteroidProjectile asteroidReference)
+    {
+        TotalScore = TotalScore + asteroidReference.pointsValue;
+        GameService.GameUI.SetScoreTextToCurrentScore(TotalScore);
+    }
+}
