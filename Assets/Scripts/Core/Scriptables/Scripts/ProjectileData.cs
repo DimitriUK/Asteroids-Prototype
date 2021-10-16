7b7f0da0b5cb313bebdfc1f4e8e4af91730a5223
Dima Zangana <zanganadima@gmail.com>
2021-10-16 15:52:32 +0300

    Add ProjectileData.cs as ScriptableObject

---
+++ Assets/Scripts/Core/Scriptables/Scripts/ProjectileData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Projectile", menuName = "Projectile Data/New Projectile", order = 51)]
+public class ProjectileData : ScriptableObject
+{
+    public enum PoolTags
+    {
+        Asteroid,    
+        Bullet
+    }
+
+    public PoolTags tag;
+    public GameObject prefab;
+    public int size;
+}
