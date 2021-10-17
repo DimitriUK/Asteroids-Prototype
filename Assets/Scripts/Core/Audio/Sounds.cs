11c90209f2fa2addef43e999f697f38a22566a27
Dima Zangana <zanganadima@gmail.com>
2021-10-17 19:55:14 +0300

    Add Sounds.cs

---
+++ Assets/Scripts/Core/Audio/Sounds.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace Core.Audio
+{
+    public class Sounds : MonoBehaviour
+    {
+        public AudioData BulletsAudioData;
+        public AudioData ExplosionAudioData;
+
+        private AudioService audioService;
+
+        private AudioClip[] BulletSoundsToPlay;
+        private AudioClip[] ExplosionSoundsToPlay;
+
+        public void ShootGun()
+        {
+            PlaySound(GetRandomBulletSound());
+        }
+
+        public void SetAsteroidDestroyedSoundAndPlay(Asteroids.AsteroidSize asteroid)
+        {
+            switch (asteroid)
+            {
+                case Asteroids.AsteroidSize.AsteroidBig:
+                    PlaySound(ExplosionSoundsToPlay[0]);
+                    break;
+                case Asteroids.AsteroidSize.AsteroidMedium:
+                    PlaySound(ExplosionSoundsToPlay[1]);
+                    break;
+                case Asteroids.AsteroidSize.AsteroidSmall:
+                    PlaySound(ExplosionSoundsToPlay[2]);
+                    break;
+            }
+        }
+
+        public AudioClip GetRandomBulletSound()
+        {
+            var random = Random.Range(0, BulletSoundsToPlay.Length);
+            return BulletSoundsToPlay[random];
+        }
+
+        private void Awake()
+        {
+            audioService = transform.parent.GetComponent<AudioService>();
+        }
+
+        private void OnValidate()
+        {
+            ExplosionSoundsToPlay = ExplosionAudioData.ClipsToPlay;
+            BulletSoundsToPlay = BulletsAudioData.ClipsToPlay;
+        }
+
+        private void PlaySound(AudioClip clipToPlay)
+        {
+            audioService.soundsAudioSource.PlayOneShot(clipToPlay);
+        }
+    }
+}
