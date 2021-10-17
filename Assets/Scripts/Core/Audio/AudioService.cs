51284ad48dbca2e4fda6c6b044a61ba7c2b6cb6b
Dima Zangana <zanganadima@gmail.com>
2021-10-17 19:54:43 +0300

    Add AudioService.cs

---
+++ Assets/Scripts/Core/Audio/AudioService.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Core.Audio
+{
+    public class AudioService : MonoBehaviour
+    {
+        public Music music;
+        public Sounds sounds;
+
+        [HideInInspector] public AudioSource musicAudioSource;
+        [HideInInspector] public AudioSource soundsAudioSource;
+
+        public void StartMusic()
+        {
+            music.StartMusicForNewGame(musicAudioSource);
+        }
+
+        private void Awake()
+        {
+            Initialise();
+        }
+
+        private void Initialise()
+        {
+            musicAudioSource = music.GetComponent<AudioSource>();
+            soundsAudioSource = sounds.GetComponent<AudioSource>();
+        }
+
+        private void OnEnable()
+        {
+            Actions.OnAsteroidDestroyed += music.IncreaseMusicSpeed;
+            Actions.OnAsteroidDestroyed += sounds.SetAsteroidDestroyedSoundAndPlay;
+            Actions.OnBulletFired += sounds.ShootGun;
+        }
+
+        private void OnDisable()
+        {
+            Actions.OnAsteroidDestroyed -= music.IncreaseMusicSpeed;
+            Actions.OnAsteroidDestroyed -= sounds.SetAsteroidDestroyedSoundAndPlay;
+            Actions.OnBulletFired -= sounds.ShootGun;
+        }
+    }
+}
