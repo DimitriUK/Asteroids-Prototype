9eeefc0e0e1f8f7aee604d431d06b0d89e5163e9
Dima Zangana <zanganadima@gmail.com>
2021-10-17 20:22:01 +0300

    Add Extensions.cs
    - Used as a extension to get the next enum

---
+++ Assets/Scripts/Extensions/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class Extensions
+{
+    public static T GetNextValue<T>(this T src) where T : struct
+    {
+        T[] EnumArray = (T[])Enum.GetValues(src.GetType());
+
+        int e = Array.IndexOf<T>(EnumArray, src) + 1;
+
+        return (EnumArray.Length == e) ? EnumArray[0] : EnumArray[e];
+    }
+}
